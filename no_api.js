4e83e834b0dabb2165435b7d23dd1a6f48c0a632
Axl Mendoza <axlmendoza02@gmail.com>
2025-02-13 22:09:50 -0600

    No api xd

---
+++ no_api.js
@@ -0,0 +1,17 @@
+{
+    "title": "Game Title",
+    "slug": "game-title",
+    "description": "Detailed game description...",
+    "score": 90,
+    "img": "https://www.metacritic.com/a/img/example.jpg",
+    "reviews": [
+      {
+        "quote": "Amazing gameplay experience.",
+        "score": 95,
+        "date": "2024-01-10",
+        "publicationName": "IGN",
+        "author": "John Doe"
+      }
+    ]
+  }
+  
